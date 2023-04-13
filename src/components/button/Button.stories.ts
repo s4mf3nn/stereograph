57f086c77d51b815c4e6d703b7ab0b6add8849db
Samfenn <samir.fennikh@gmail.com>
2023-04-13 20:07:33 +0200

    Project init with typescript, tailwind and storybook.

---
+++ src/components/button/Button.stories.ts
@@ -0,0 +1,15 @@
+import Button from "./Button";
+
+export default {
+  title: 'Collections/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {},
+};
+
+export const Primary = {
+  args: {
+    variant: 'primary',
+    label: 'Rechercher',
+  },
+};
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="text-3xl">
+        Hello Stereograph!
+      </h1>
+    </div>
+  );
+}
